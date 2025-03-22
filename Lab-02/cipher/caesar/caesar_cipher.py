48211eefec07c19fcf737388271af6e996f216c5
LeQuocKhanh1710 <lequockhanh17102004.9a4@gmail.com>
2025-03-22 18:36:50 +0700

    Update Lab3

--- Lab-02/cipher/caesar/caesar_cipher.py
+++ Lab-02/cipher/caesar/caesar_cipher.py
@@ -1,9 +1,11 @@
 from cipher.caesar import ALPHABET
+
 class CaesarCipher:
     def __init__(self):
         self.alphabet = ALPHABET
-    def encrypt_text(self,text: str,key: int) -> str:
-        alphabet_len = len (self.alphabet)
+        
+    def encrypt_text(self, text: str, key: int) -> str:
+        alphabet_len = len(self.alphabet)
         text = text.upper()
         encrypted_text = []
         for letter in text:
@@ -12,8 +14,9 @@ class CaesarCipher:
             output_letter = self.alphabet[output_index]
             encrypted_text.append(output_letter)
         return "".join(encrypted_text)
-    def decrypt_text(self,text: str,key: int) -> str:
-        alphabet_len = len (self.alphabet)
+    
+    def decrypt_text(self, text: str, key: int) -> str:
+        alphabet_len = len(self.alphabet)
         text = text.upper()
         decrypted_text = []
         for letter in text:
